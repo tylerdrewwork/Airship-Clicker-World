6970b8e70fb66e97bc2ce14b333ff5a4d04c6fd9
tylerdrewwork <33964795+tylerdrewwork@users.noreply.github.com>
2026-02-21 15:27:31 -0600

    update scorekeeper's network functions

--- Assets/Code/Gameplay/ScoreKeeper.ts
+++ Assets/Code/Gameplay/ScoreKeeper.ts
@@ -1,6 +1,7 @@
 import { Platform } from "@Easy/Core/Shared/Airship";
 import { Game } from "@Easy/Core/Shared/Game";
 import { AirshipNetworkBehaviour } from "@Easy/Core/Shared/Network/AirshipNetworkBehaviour";
+import { NetworkFunction } from "@Easy/Core/Shared/Network/NetworkFunction";
 import { NetworkSignal } from "@Easy/Core/Shared/Network/NetworkSignal";
 import { Command } from "@Easy/Core/Shared/Network/ServerRpc";
 import { Player } from "@Easy/Core/Shared/Player/Player";
@@ -8,46 +9,73 @@ import { Player } from "@Easy/Core/Shared/Player/Player";
 export default class ScoreKeeper extends AirshipBehaviour {
 
 	private static readonly GLOBAL_CLICK_DATA_KEY = `Clicks-Global`;
-	private readonly clicksInBatch = 0;
+	private clicksInBatch = 0;
+	private lastBatchTime = 0;
+	/// Every `BATCH_INTERVAL` seconds the score will update from the datastore
+	private static readonly BATCH_INTERVAL = 5; // In Seconds
 
-	private addClicks = new NetworkSignal<{ clicks: number }>("AddClicks");
+	// NetworkFunction's generics:
+	// first generic argument is the object that the client fires to the server
+	// second generic argument is the object that is returned via callback
+	private addClicks = new NetworkFunction<{ clicks: number }, GlobalClickData>("AddClicks");
 
 
 	override Start(): void {
 		print("Hello, World! from ScoreKeeper!");
 
 		if (Game.IsServer()) {
-			this.addClicks.server.OnClientEvent((player, event) => {
-				this.AddScoreToDataStoreInBatch(player, event.clicks);
+			this.addClicks.server.SetCallback((player, event) => {
+				let clicksResult = 0;
+				
+				// 
+				this.S_AddScoreToDataStoreInBatch(player, event.clicks).then(res => {
+					if (res) clicksResult = res.clicks;
+				});
+
+				// NetworkFunction needs a GlobalClickData
+				return {
+					clicks: clicksResult
+				}
 			})
 		}
 	}
 
-	public AddClicks(clicks: number): void {
-		this.RequestAddClicks(clicks);
+	override Update(): void {
+		// run every 5 seconds
+		if (Game.IsClient() && Time.timeSinceLevelLoad - this.lastBatchTime >= ScoreKeeper.BATCH_INTERVAL) {
+			this.lastBatchTime = Time.timeSinceLevelLoad;
+			this.C_RequestAddClicks();
+			
+		}
 	}
 
-	public RequestAddClicks(clickAmt: number): void {
+	public AddClickLocal(clicks: number): void {
+		this.clicksInBatch++;
+	}
+
+	public C_RequestAddClicks(): void {
 		if (!Game.IsClient()) return;
 
 		// Client -> Server
 		// Network Signal 
-		this.addClicks.client.FireServer({ clicks: clickAmt });
+		let newGlobalClicks = this.addClicks.client.FireServer({ clicks: this.clicksInBatch });
+		this.clicksInBatch = 0;
 	}
 
 	@Server()
-	private async AddScoreToDataStoreInBatch(player : Player, clicksAmt : number) {
+	private async S_AddScoreToDataStoreInBatch(player : Player, clicksAmt : number) : Promise<GlobalClickData | undefined> {
 		if (!Game.IsServer()) return;
 
 		const playerClickDataKey = `Clicks-Player:${player.userId}`;
 
-		// Get from datastore
+		// Get Global clicks from datastore
 		const clicksGlobal = await Platform.Server.DataStore.GetKey<GlobalClickData>(
 			ScoreKeeper.GLOBAL_CLICK_DATA_KEY,
 		);
 		let globalCurrentClicks = 0;
 		if (clicksGlobal) globalCurrentClicks = clicksGlobal.clicks;
 
+		// Get Player clicks
 		const clicksPlayer = await Platform.Server.DataStore.GetKey<PlayerClickData>(playerClickDataKey);
 		let playerCurrentClicks = 0; // init at 0 to create new key if needed
 		if (clicksPlayer) playerCurrentClicks = clicksPlayer.clicks;
@@ -62,6 +90,16 @@ export default class ScoreKeeper extends AirshipBehaviour {
 
 		await Platform.Server.DataStore.SetKey(playerClickDataKey, playerClickData);
 		await Platform.Server.DataStore.SetKey(ScoreKeeper.GLOBAL_CLICK_DATA_KEY, globalClickData);
+		return globalClickData;
+	}
+
+	/* This is where clicks can be visually updates when pulling from the Server
+	* Since we retrieve data every 5 seconds, we can simulate the clicks on the button
+	* from other players over 5 seconds.
+	*/
+	private UpdateClicksVisuals() : void {
+		// TODO add cool visuals. For now, just update the text
+		
 	}
 }
 
