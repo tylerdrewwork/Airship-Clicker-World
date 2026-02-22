8a75fcc4373a998142f20edbd7b83685aecf4dae
tylerdrewwork <33964795+tylerdrewwork@users.noreply.github.com>
2026-02-22 10:52:49 -0600

    adds copyairshipdebuginfo

---
+++ Assets/Editor/CopyAirshipDebugInfo/CopyAirshipDebugInfo.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+public static class CopyAirshipDebugInfo
+{
+    const string AirshipPackageName = "gg.easy.airship";
+
+    [MenuItem("Airship/Copy Debug Info")]
+    public static void CopyDebugInfo()
+    {
+        string airshipVersion = "unknown";
+        foreach (var p in PackageInfo.GetAllRegisteredPackages())
+        {
+            if (p.name == AirshipPackageName)
+            {
+                airshipVersion = p.version;
+                break;
+            }
+        }
+
+        string info = string.Format(
+            "CPU: {0}\nGPU: {1}\nUnity: {2}\nDevice: {3}\nDevice type: {4}\nRAM: {5} MB\nAirship: {6}",
+            SystemInfo.processorType,
+            SystemInfo.graphicsDeviceName,
+            Application.unityVersion,
+            SystemInfo.deviceModel,
+            SystemInfo.deviceType,
+            SystemInfo.systemMemorySize,
+            airshipVersion
+        );
+        EditorGUIUtility.systemCopyBuffer = info;
+        Debug.Log("Debug info copied to clipboard.");
+    }
+}
