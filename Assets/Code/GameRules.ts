b1b9e541a57611d1f99ec102d821ff083f34590e
tylerdrewwork <33964795+tylerdrewwork@users.noreply.github.com>
2026-02-22 14:14:30 -0600

    fixes most of the color issues

--- Assets/Code/GameRules.ts
+++ Assets/Code/GameRules.ts
@@ -5,6 +5,7 @@ import { Bin } from "@Easy/Core/Shared/Util/Bin";
 import ScoreKeeper from "./Gameplay/ScoreKeeper";
 import ClickVisuals from './Gameplay/ClickVisuals';
 import { NetworkSignal } from "@Easy/Core/Shared/Network/NetworkSignal";
+import CustomCharacterController from "./Gameplay/CustomCharacterController";
 
 export default class GameRules extends AirshipSingleton {
 
@@ -14,25 +15,49 @@ export default class GameRules extends AirshipSingleton {
 	public possibleColors: Color[];
 
 	public mainCamera: Camera;
-	
+
 	private bin = new Bin();
 
 	// TODO move this to a better place, putting it here because I'm trying to finish this project this weekend
-	public onCustomCharacterInstantiated = new NetworkSignal<{color: Color}>("OnCustomCharacterInstantiated");
-	public syncCustomCharacterColors = new NetworkSignal<{color: Color, userId: string, username: string}>("SyncCustomCharacterColors");
+	public onCustomCharacterInstantiated = new NetworkSignal<{}>("OnCustomCharacterInstantiated");
+	public syncCustomCharacterColors = new NetworkSignal<{ color: Color, userId: string, username: string }>("SyncCustomCharacterColors");
 
 	override Start(): void {
 
-		if (Game.IsClient()) {			
+		if (Game.IsClient()) {
 			// Prevent the mouse from being locked
 			this.bin.Add(() => {
 				Mouse.ClearAllUnlockers();
 			});
 
 			Airship.Camera.SetEnabled(false);
-			Mouse.SetCursorVisible(true);
+			Mouse.SetCursorVisible(false);
 
 			Mouse.AddUnlocker();
+		}
+
+		if (Game.IsServer()) {
+			let onCustomCharacterInitialized = GameRules.Get().onCustomCharacterInstantiated;
+			// send character info to other players on client character initialize
+			onCustomCharacterInitialized.server.OnClientEvent((player, event) => {
+				print("Player Character initialized. Username: " + player.username);
+
+				let pchar = player.character?.gameObject.GetAirshipComponent<CustomCharacterController>();
+				if (pchar) {
+
+					if (!pchar.cursorColor) {
+						let randomColorIndex = math.random(0, GameRules.Get().possibleColors.size() - 1);
+						let color = GameRules.Get().possibleColors[randomColorIndex];
+						pchar.cursorColor = color;
+					}
+
+					GameRules.Get().syncCustomCharacterColors.server.FireAllClients({
+						color: pchar.cursorColor,
+						userId: player.userId,
+						username: player.username
+					})
+				}
+			});
 		}
 	}
 }
--- Assets/Code/Gameplay/CustomCharacterController.ts
+++ Assets/Code/Gameplay/CustomCharacterController.ts
@@ -14,34 +14,30 @@ export default class CustomCharacterController extends AirshipBehaviour {
 
 	protected Start(): void {
 		
+		
 		if (Game.IsClient()) {
+			// assign random color
+			// let randomColorIndex = math.random(0, GameRules.Get().possibleColors.size() - 1);
+			// this.cursorColor = GameRules.Get().possibleColors[randomColorIndex];
+
 			// register to other characters syncing
 			GameRules.Get().syncCustomCharacterColors.client.OnServerEvent(event => {
 				let syncedPlayer = Airship.Players.FindByUserId(event.userId);
 				let customChar = syncedPlayer?.character?.gameObject.GetAirshipComponent<CustomCharacterController>();
 				if (customChar) {
-					customChar.UpdateColorAndName(event.color, event.username);
-					// let usernameTmp = customChar.gameObject.GetComponentInChildren<TextMeshProUGUI>();
-					// customChar.cursorColor = event.color;
-					// if (usernameTmp) usernameTmp.text = event.username
+					// customChar.UpdateColorAndName(event.color, event.username);
+					print("event color & user" + event.color + event.username)
+					let usernameTmp = customChar.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+					let sr = customChar.gameObject.GetComponentInChildren<SpriteRenderer>();
+					print("sr and tmp" + sr + usernameTmp)
+
+					if (sr) sr.color = event.color;
+					if (usernameTmp) usernameTmp.text = event.username
 				}
 			})
 		}
 		
-		if (Game.IsServer()) {
-			let onCustomCharacterInitialized = GameRules.Get().onCustomCharacterInstantiated;
-			// send character info to other players on client character initialize
-			onCustomCharacterInitialized.server.OnClientEvent((player, event) => {
-				let pchar = player.character?.gameObject.GetAirshipComponent<CustomCharacterController>();
-				if (pchar) {
-					GameRules.Get().syncCustomCharacterColors.server.FireAllClients({
-						color: pchar.cursorColor,
-						userId: player.userId,
-						username: player.username
-					})
-				}
-			});
-		}
+		
 
 		if (Game.IsClient()) this.C_Initialize();
 	}
@@ -54,9 +50,7 @@ export default class CustomCharacterController extends AirshipBehaviour {
 	private C_Initialize() {
 		let onCustomCharacterInitialized = GameRules.Get().onCustomCharacterInstantiated;
 
-		// assign random color
-		let randomColorIndex = math.random(0, GameRules.Get().possibleColors.size() - 1);
-		this.cursorColor = GameRules.Get().possibleColors[randomColorIndex];
+		
 
 		// Check if we are owner of this character
 		if (Game.localPlayer.character?.gameObject.GetAirshipComponent<CustomCharacterController>() === this) {
@@ -67,27 +61,27 @@ export default class CustomCharacterController extends AirshipBehaviour {
 			let sr = this.gameObject.GetComponentInChildren<SpriteRenderer>();
 			if (sr) {
 				sr.sortingOrder = 10;
+				// sr.color = this.cursorColor;
 			}
 
 			this.UpdateColorAndName(this.cursorColor, Game.localPlayer.username)
+			// add our username
+			let usernameText = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+			if (usernameText) {
+				usernameText.text = Game.localPlayer.username;
+				// usernameText.color = this.cursorColor;
+			}
+
+			
+			Mouse.SetCursorVisible(false);
+			
+			onCustomCharacterInitialized.client.FireServer({});
 		}
 		else this.isOwner = false;
-
-		Mouse.SetCursorVisible(false);
-
-		onCustomCharacterInitialized.client.FireServer({ color: this.cursorColor });
 	}
 
 	public UpdateColorAndName(color: Color, name: string) {
-		// add our username
-		let usernameText = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
-		if (usernameText) {
-			usernameText.text = name;
-			usernameText.color = color;
-		}
-
-		let sr = this.gameObject.GetComponentInChildren<SpriteRenderer>();
-		if (sr) sr.color = color;
+		
 	}
 
 	private FollowCursor() {
