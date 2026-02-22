4dcdf4ac2d7a5c290b6f725dddf9a69401bae56f
tylerdrewwork <33964795+tylerdrewwork@users.noreply.github.com>
2026-02-22 13:47:25 -0600

    adds major updates to UI

--- Assets/Code/GameRules.ts
+++ Assets/Code/GameRules.ts
@@ -4,15 +4,22 @@ import { Mouse } from "@Easy/Core/Shared/UserInput/Mouse";
 import { Bin } from "@Easy/Core/Shared/Util/Bin";
 import ScoreKeeper from "./Gameplay/ScoreKeeper";
 import ClickVisuals from './Gameplay/ClickVisuals';
+import { NetworkSignal } from "@Easy/Core/Shared/Network/NetworkSignal";
 
 export default class GameRules extends AirshipSingleton {
 
 	public scoreKeeper: ScoreKeeper;
 	public clickVisuals: ClickVisuals;
 
+	public possibleColors: Color[];
+
 	public mainCamera: Camera;
 	
 	private bin = new Bin();
+
+	// TODO move this to a better place, putting it here because I'm trying to finish this project this weekend
+	public onCustomCharacterInstantiated = new NetworkSignal<{color: Color}>("OnCustomCharacterInstantiated");
+	public syncCustomCharacterColors = new NetworkSignal<{color: Color, userId: string, username: string}>("SyncCustomCharacterColors");
 
 	override Start(): void {
 
--- Assets/Code/Gameplay/CustomCharacterController.ts
+++ Assets/Code/Gameplay/CustomCharacterController.ts
@@ -1,17 +1,106 @@
+import { Game } from "@Easy/Core/Shared/Game";
+import { Mouse } from "@Easy/Core/Shared/UserInput";
+import GameRules from "Code/GameRules";
+import { NetworkFunction } from "@Easy/Core/Shared/Network/NetworkFunction";
+import { NetworkSignal } from "@Easy/Core/Shared/Network/NetworkSignal";
+import { Airship } from '../../AirshipPackages/@Easy/Core/Shared/Airship';
+
 export default class CustomCharacterController extends AirshipBehaviour {
 
-	public canvasParent: Transform;
+	public cursorRect: RectTransform;
+	private isOwner: boolean;
+	public cursorColor: Color;
 
-	private spawnTime: number;
 
-	override Start(): void {
-		this.spawnTime = Time.time;
+	protected Start(): void {
+		
+		if (Game.IsClient()) {
+			// register to other characters syncing
+			GameRules.Get().syncCustomCharacterColors.client.OnServerEvent(event => {
+				let syncedPlayer = Airship.Players.FindByUserId(event.userId);
+				let customChar = syncedPlayer?.character?.gameObject.GetAirshipComponent<CustomCharacterController>();
+				if (customChar) {
+					customChar.UpdateColorAndName(event.color, event.username);
+					// let usernameTmp = customChar.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+					// customChar.cursorColor = event.color;
+					// if (usernameTmp) usernameTmp.text = event.username
+				}
+			})
+		}
+		
+		if (Game.IsServer()) {
+			let onCustomCharacterInitialized = GameRules.Get().onCustomCharacterInstantiated;
+			// send character info to other players on client character initialize
+			onCustomCharacterInitialized.server.OnClientEvent((player, event) => {
+				let pchar = player.character?.gameObject.GetAirshipComponent<CustomCharacterController>();
+				if (pchar) {
+					GameRules.Get().syncCustomCharacterColors.server.FireAllClients({
+						color: pchar.cursorColor,
+						userId: player.userId,
+						username: player.username
+					})
+				}
+			});
+		}
+
+		if (Game.IsClient()) this.C_Initialize();
 	}
 
 	override Update(): void {
-		// for the first 3 seconds, force transform parent onto the canvasParent
-		if (this.canvasParent && Time.time - this.spawnTime < 3) {
-			this.transform.parent = this.canvasParent;
+		if (Game.IsClient() && this.isOwner) this.FollowCursor();
+	}
+
+	@Client()
+	private C_Initialize() {
+		let onCustomCharacterInitialized = GameRules.Get().onCustomCharacterInstantiated;
+
+		// assign random color
+		let randomColorIndex = math.random(0, GameRules.Get().possibleColors.size() - 1);
+		this.cursorColor = GameRules.Get().possibleColors[randomColorIndex];
+
+		// Check if we are owner of this character
+		if (Game.localPlayer.character?.gameObject.GetAirshipComponent<CustomCharacterController>() === this) {
+			this.isOwner = true;
+			print("is owner?" + this.isOwner)
+
+			// make our mouse topmost
+			let sr = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+			if (sr) {
+				sr.sortingOrder = 10;
+			}
+
+			this.UpdateColorAndName(this.cursorColor, Game.localPlayer.username)
 		}
+		else this.isOwner = false;
+
+		Mouse.SetCursorVisible(false);
+
+		onCustomCharacterInitialized.client.FireServer({ color: this.cursorColor });
+	}
+
+	public UpdateColorAndName(color: Color, name: string) {
+		// add our username
+		let usernameText = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+		if (usernameText) {
+			usernameText.text = name;
+			usernameText.color = color;
+		}
+
+		let sr = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+		if (sr) sr.color = color;
+	}
+
+	private FollowCursor() {
+		// print("following")
+		//Point the character towards the mouse
+		let mousepos = Mouse.GetPositionVector3();
+		let newPos = GameRules.Get().mainCamera.ScreenToWorldPoint(mousepos);
+
+		//Move our custom cursor graphic
+		this.cursorRect.position = new Vector3(
+			newPos.x,
+			newPos.y,
+			0
+		);
 	}
 }
